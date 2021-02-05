2ae7e61e5a623194bcf8ee8845ec0e71f9424825
nurfadillahcloudcomputing <78609244+nurfadillahcloudcomputing@users.noreply.github.com>
2021-02-05 08:12:44 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,103 @@
+<html>
+<head><font color="WHITE">
+<h1>M E N U</h1>
+</head>
+<form action="#" style="width:700px class="posisi";>
+	<fieldset class="h"/>
+	<table style="width: 600px;">
+	<body background="image2.jpg">
+
+
+<font color="WHITE">
+<hr><b>MOTIVASI</b>
+<marquee><b><i><font color="RED">
+<p>BERUSAHA SELAGI MUDAH,</p>
+<p>Lebih baik kita berkeringat hari ini daripada kita harus bercucuran air mata di masa depan </p>
+<p>Dia yang gagal menguras keringat di masa muda Harus bersiap mengeluarkan air mata lebih banyak di masa tuanya</p>
+<p>Dia yang berhasil menahan diri dari hura-hura lebih layak untuk merayakan kesuksesannya di masa depan</p>
+<p>PAIN TODAY! PRIDE TOMORROW! </p>
+<p>Bersakit-sakit hari ini,Bersusah-susah hari ini </p>
+<p>Tidak ada masalah! rasakan sakitnya! </p>
+<p>Nikmati perihnya!</p>
+<p>Sehingga ada hal yang kita banggakan di esok hari :) </p></i></b></marquee> 
+<HR>
+<center>
+<font size="25px">
+<b>DATA PRIBADI</b>
+<HR>
+</font>
+</center>
+<p>
+<tr>
+<td><b>
+<i>NAMA</i></b></Td>
+<td><i><b> :NURFADILLAH</i></b></Td>
+</tr>
+<p>
+<tr>
+<td><b><i>NIM </i></b></Td>
+<td><i><b> :217 280 090 </i></b></Td>
+</tr>
+<p>
+<tr>
+<td><b><i>JENIS KELAMIN  </i></b></Td>
+<td><b><i> : Perempuan </i></b></Td>
+</tr>
+<p>
+<tr>
+<td><b><i>TEMPAT TANGGAL LAHIR  </i></b></Td>
+<td><b><i>: Malaysia, 18 November 1998 </i></b></Td>
+</tr>
+<p>
+<tr>
+<td><b><i>AGAMA  </i></b></Td>
+<td><b><i>: Islam </i></b></Td>
+</tr>
+<p>
+<tr>
+<td><b><i>ASAL DAERAH</i></b></Td>
+<td><b><i>: Desa Sabbangparu, Kec.LEMBANG, Kab.Pinrang </i></b></Td>
+</tr>
+<p>
+<tr>
+<td><b><i>ALAMAT DOMINSILI </i></b></Td>
+<td><b><i>: Jl.Pelabuhan, Kajuangin </i></b></Td>
+</tr>
+<p>
+<tr>
+<td><b><i>HOBI			 </i></b></Td>
+<td><b><i>: Membaca, Nonton, Jalan-jalan </i></b></Td>
+</tr>
+<p>
+<tr>
+<td><b><i>JUMLAH SAUDARA	 </i></b></Td>
+<td><b><i>: 7 </i></b></Td></tr>
+<p>
+<tr>
+<td><b><i>ANAK KE		 </i></b></Td>
+<td><b><i> : 4</i></b></Td>
+</tr>
+<p>
+<tr>
+<td><b><i>EMAIL		 </i></b></Td>
+<td><b><i>: nurfadillah.rustamdalik27.090@gmail.com</i></b></Td>
+</tr>
+<p>
+<tr>
+<td><b><i>FACEBOOK		</i></b></Td>
+<td><b>
+<i>: Nurfadillah </i></b></Td>
+</tr>
+<p>
+<tr>
+<td><b><i>INSTAGRAM		 </i></b></Td>
+<td><b><i>: bungarevolusi_611</i></b></Td>
+</tr>
+
+<tr>
+<td rowspan="10"width="100px">
+<img src="fhoto.jpg"width="150px"height="200px"border"5">
+</td>
+</tr>
+<body>
+</html>
